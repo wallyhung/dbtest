c167e922f446979095061c6ec614b19fe2063e40
wally <hongwei200612@gmail.com>
2013-12-26 10:45:34 +0800

    update

--- dbtest/src/com/wally/login58/WuBa.java
+++ dbtest/src/com/wally/login58/WuBa.java
@@ -73,7 +73,6 @@ public class WuBa {
                   HashMap<String, String> header = new HashMap<String, String>();
                   header.put("Cookie",cookie);
                   
-                  //测试不成功
                    
                   //登陆我的中心 验证是否登陆成功！
                  System.out.println(EntityUtils.toString(SendRequest.sendGet("http://my.58.com/", header, null, "utf-8").getHttpEntity(),"utf-8"));
